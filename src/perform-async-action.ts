e872b02b12d555e989a70c561ec77cf3054098c3
dpinart <dpinart@newuibcn.com>
2018-11-08 13:12:31 +0100

    feat: performAsyncAction feature added

---
+++ src/perform-async-action.ts
@@ -0,0 +1,20 @@
+import {Store} from "redux";
+import {createAsyncActions} from "./create-async-actions";
+
+export async function performAsyncAction<T = any>(actionName: string, fn: () => Promise<T>, store: Store, meta?: any, errorHandler?: (error) => Promise<any>) {
+    let started = window.performance.now();
+    let actions = createAsyncActions(actionName);
+    try {
+        store.dispatch({type: actions.started, meta: meta});
+        let payload = await fn();
+        store.dispatch({type: actions.succeeded, payload, meta});
+    }
+    catch (e) {
+        if(errorHandler)
+            await errorHandler(e);
+        store.dispatch({type: actions.failed, payload: e, meta});
+    }
+    finally {
+        store.dispatch({type: actions.ended, payload: {elapsed: window.performance.now() - started}, meta});
+    }
+}
