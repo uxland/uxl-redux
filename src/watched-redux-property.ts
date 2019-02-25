cb1241ca47fe894b173c11b970d130ab9a09668b
dpinart <dpinart@newuibcn.com>
2019-02-25 12:34:39 +0100

    fix: bug fixed with composition

--- src/watched-redux-property.ts
+++ src/watched-redux-property.ts
@@ -1,23 +1,7 @@
 import {WatchOptions} from "./watch";
-import {Store} from "redux";
 import {collect} from "@uxland/uxl-utilities";
 
-const DEFAULT_STORE_PROPERTY = 'reduxDefaultStore';
 const WATCHED_PROPERTIES_PROPERTY = 'watchedReduxProperties';
-export const getDefaultStore = (proto: any) =>{
-    return proto.constructor.reduxDefaultStore;
-};
-
-
-export const setDefaultStore = (proto: any, store: Store) =>{
-    Object.defineProperty(proto.constructor, DEFAULT_STORE_PROPERTY, {
-        get() {
-            return store;
-        },
-        enumerable: true,
-        configurable: true
-    });
-};
 
 export const createWatchedReduxProperty = (propConfig: WatchOptions, proto: any, propName: string) =>{
     const properties =  Object.keys(Object.assign({}, proto.constructor.watchedReduxProperties))
