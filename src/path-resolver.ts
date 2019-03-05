496bafee1626f066b6a32e2980336be054da2a77
danielcabiscol <danielcabiscol@gmail.com>
2019-03-05 11:20:00 +0100

    fix: fix date-fns version

--- src/path-resolver.ts
+++ src/path-resolver.ts
@@ -11,5 +11,4 @@ export interface PathResolver {
 
 export type Resolver = (action: Action) => Lens;
 export const factory =  (resolver: Resolver) => <PathResolver>{resolver};
-
 export const resolvePath: (path: Lens | PathResolver, action?: Action) => Lens = (path, action) =>  ifElse(is(Function), identity ,(pr: PathResolver) => pr.resolver(action))(path);
